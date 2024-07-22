d72e238eda94208228060940add13e64cbd80ec0
2R <haih70872@gmail.com>
2024-07-22 13:14:31 +0700

    sửa hiển thị bill

--- src/main/java/com/example/demo/restController/nguyen/NBillRestController.java
+++ src/main/java/com/example/demo/restController/nguyen/NBillRestController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -55,7 +56,7 @@ public class NBillRestController {
                                @RequestParam(required = false) Integer status,
                                @RequestParam(defaultValue = "0") int page,
                                @RequestParam(defaultValue = "5") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
         return billService
                 .searchBills(code, customerName, phoneNumber, typeBill, startDate, endDate, status,
                         pageable);
