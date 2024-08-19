4a492c377b119bbe5d6064ac7d44a5a2598ee216
2R <haih70872@gmail.com>
2024-08-20 03:05:18 +0700

    sửa lại point cho customer

--- src/main/java/com/example/demo/restController/point/PointSettingsController.java
+++ src/main/java/com/example/demo/restController/point/PointSettingsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/admin/point-settings")
@@ -31,4 +33,6 @@ public class PointSettingsController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+
 }
