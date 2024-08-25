bb33893d934f45b0d2e0804cec49a922f4031b47
2R <haih70872@gmail.com>
2024-08-25 13:59:53 +0700

    fix voucher

--- src/main/java/com/example/demo/service/onlineShop/impl/OLBillServiceImpl2.java
+++ src/main/java/com/example/demo/service/onlineShop/impl/OLBillServiceImpl2.java
@@ -120,7 +120,7 @@ public class OLBillServiceImpl2 implements OLBillService2 {
             if (existingVoucher.getStatus() == 1 && existingVoucher.getQuantity() > 1) {
                 if (existingVoucher.getApplyfor() == 1) {
                     Optional<CustomerType> customerType = customerTypeRepository.findByCustomersId(customer.getId());
-                    if ( customerType.isPresent() || customerType.get().getStatus() != 1) {
+                    if ( customerType.isPresent() && customerType.get().getStatus() != 1) {
                         return ResponseEntity.ok(3); // Add a different response code to handle this case
                     }
                 }
